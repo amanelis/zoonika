e83ee680d6fedc098e7bfb48ee4c1f529e92c3da
Alex Manelis <amanelis@gmail.com>
2010-07-07 01:01:13 -0700

    README update

--- config/config.inc.php
+++ config/config.inc.php
@@ -9,5 +9,5 @@ $bot_password = 'alex18257';
 //Your database credentials to utilize bot, quotes..
 $db_host	= 'internal-db.s51949.gridserver.com';
 $db_user	= 'db51949';
-$db_pass	= 'T9FwMVM4';
+$db_pass	= '5432@1gxy+V_}';
 $database	= 'db51949_ztbot';
